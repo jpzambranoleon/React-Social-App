83bc151265594e56fc9bd6bf0ac26575314016c7
Jean-Paul <jpzambrano.leon@gmail.com>
2021-12-21 22:23:04 -0600

    updated Share.jsx

--- src/components/share/Share.jsx
+++ src/components/share/Share.jsx
@@ -1,4 +1,5 @@
 import "./share.css"
+import { PermMedia } from "@material-ui/icons"
 
 export default function Share() {
     return (
@@ -6,8 +7,17 @@ export default function Share() {
             <div className="shareWrapper">
                 <div className="shareTop">
                     <img className="shareProfileImg" src="/assets/person/person1.jpg" alt="" />
+                    <input placeholder="What's on your mind Emma?" className="shareInput" />
                 </div>
-                <div className="shareBottom"></div>
+                <hr className="shareHr"/>
+                <div className="shareBottom">
+                    <div className="shareOptions">
+                        <div className="shareOption">
+                            <PermMedia className="shareIcon"/>
+                            <span className="shareOptionText">Photo or Video</span>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
