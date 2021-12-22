b1ca88b879479cb8697bfe2b5c1585257b283328
Jean-Paul <jpzambrano.leon@gmail.com>
2021-12-22 17:55:26 -0600

    updated files

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -3,13 +3,24 @@ import "./rightbar.css"
 export default function RightBar() {
     return (
         <div className="rightbar">
-            <div className="rightWrapper">
+            <div className="rightbarWrapper">
                 <div className="birthdayContainer">
                     <img className="birthdayImg" src="assets/gift.png" alt="" />
                     <span className="birthdayText">
-                        <b>Aki Majima</b> and <b>3 other friends</b> have a birthday today
+                        <b>Aki Majima</b> and <b>3 other friends</b> have a birthday today.
                     </span>
                 </div>
+                <img className="rightbarAd" src="assets/ad.png" alt="" />
+                <h4 className="rightbarTitle">Online Friends</h4>
+                <ul className="rightbarFriendList">
+                    <li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img className="rightbarProfileImg" src="assets/person/person4.jpg" alt="" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUsername">Winnie Hale</span>
+                    </li>
+                </ul>
             </div>
         </div>
     )
