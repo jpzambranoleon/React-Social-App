b8854a94d8f06ceb5a11e515b51f3209ecce8111
Jean-Paul <jpzambrano.leon@gmail.com>
2021-12-21 22:08:41 -0600

    updated Feed.jsx

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -3,7 +3,7 @@ import "./feed.css"
 export default function Feed() {
     return (
         <div className="feed">
-            feed   
+            <div className="feedWrapper"></div>   
         </div>
     )
 }
