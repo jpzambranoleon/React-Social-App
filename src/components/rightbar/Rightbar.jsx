da590e358703331c898e132e766064925a582191
Jean-Paul <jpzambrano.leon@gmail.com>
2021-12-22 11:17:02 -0600

    updated Rightbar.jsx

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -3,7 +3,14 @@ import "./rightbar.css"
 export default function RightBar() {
     return (
         <div className="rightbar">
-            rightbar
+            <div className="rightWrapper">
+                <div className="birthdayContainer">
+                    <img className="birthdayImg" src="assets/gift.png" alt="" />
+                    <span className="birthdayText">
+                        <b>Aki Majima</b> and <b>3 other friends</b> have a birthday today
+                    </span>
+                </div>
+            </div>
         </div>
     )
 }
