0a2b088af6be21c90b82e61ecc4342934d413b79
Jean-Paul <jpzambrano.leon@gmail.com>
2021-12-21 18:36:58 -0600

    updated Topbar.jsx

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,9 +1,30 @@
 import "./topbar.css"
+import { Search, Person } from "@material-ui/icons"
 
 export default function Topbar() {
     return (
-        <div>
-            topbar
+        <div className="topbarContainer">
+            <div className="topBarLeft">
+                <span className="logo">someSocial</span>
+            </div>
+            <div className="topBarCenter">
+                <div className="searchbar">
+                    <Search />
+                    <input placeholder="Search for friends, post or video" className="searchInput" />
+                </div>
+            </div>
+            <div className="topBarRight">
+                <div className="topbarLinks">
+                    <span className="topbarLink">Homepage</span>
+                    <span className="topbarLink">Timeline</span>
+                </div>
+                <div className="topbarIcons">
+                    <div className="topbarIconItem">
+                        <Person />
+                        <span className="topbarIconBadge">1</span>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
